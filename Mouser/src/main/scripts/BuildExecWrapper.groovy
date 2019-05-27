2dbd643b4d9adde37e28a831d199c6f82e710cc3
Jochen Peuker <jochen.peuker@binaris-informatik.de>
2019-05-27 20:56:37 +0200

    bugfix: groovy dependencies, script template

--- Mouser/src/main/scripts/BuildExecWrapper.groovy
+++ Mouser/src/main/scripts/BuildExecWrapper.groovy
@@ -1,13 +1,11 @@
-import groovy.text *
-
 println "reading file wrap"
 String bashWrapCmdFile = new File("${project.basedir}/src/main/scripts/wrap".toString()).getText()
 
 println "replacing project name"
-def bashWrapCmdFileText = new SimpleTemplateEngine().createTemplate(bashWrapCmdFile).make([fileName: project.build.finalName])
+def bashWrapCmdFileText = new groovy.text.SimpleTemplateEngine().createTemplate(bashWrapCmdFile).make([fileName: "${project.build.finalName}"])
 
 println "creating file createBashWrapper.sh"
-File createBashWrapperFile = new File("${project.basedir}/output/createBashWrapper.sh".toString())
+File createBashWrapperFile = new File("${project.basedir}/src/main/scripts/createBashWrapper.sh".toString())
 
 println "writing to file"
 createBashWrapperFile.withWriter('UTF-8') {writer -> writer.write(bashWrapCmdFileText)}
