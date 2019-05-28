8fadf805a6a1926841683f4c913dabb65df6a277
Jochen Peuker <jochen.peuker@binaris-informatik.de>
2019-05-29 01:21:26 +0200

    fixed: automatically create runner for linux systems

--- Mouser/src/main/scripts/BuildExecWrapper.groovy
+++ Mouser/src/main/scripts/BuildExecWrapper.groovy
@@ -1,11 +1,25 @@
-println "reading file wrap"
-String bashWrapCmdFile = new File("${project.basedir}/src/main/scripts/wrap".toString()).getText()
+//println "reading file wrapperCmdTemplate"
+//String bashWrapCmdFile = new File("${project.basedir}/src/main/scripts/wrapperCmdTemplate".toString()).getText()
+//
+//println "replacing project name"
+//def bashWrapCmdFileText = new groovy.text.SimpleTemplateEngine().createTemplate(bashWrapCmdFile).make([fileName: "${project.build.finalName}"])
+//
+//println "creating file createBashWrapper.sh"
+//File createBashWrapperFile = new File("${project.basedir}/src/main/scripts/createBashWrapper.sh".toString())
+//
+//println "writing to file"
+//createBashWrapperFile.withWriter('UTF-8') {writer -> writer.write(bashWrapCmdFileText)}
+//
+//println "execute createBashWrapper.sh"
+//builder = new AntBuilder()
+//builder.chmod(file:"${project.basedir}/src/main/scripts/createBashWrapper.sh", perm:"+x")
+//createBashWrapperFile.getText().execute() //problem with cat command execution
 
-println "replacing project name"
-def bashWrapCmdFileText = new groovy.text.SimpleTemplateEngine().createTemplate(bashWrapCmdFile).make([fileName: "${project.build.finalName}"])
+File bashCmdFile = new File("${project.basedir}/src/main/scripts/stub.sh".toString())
+File jarFile = new File("${project.basedir}/target/${project.build.finalName}-jar-with-dependencies.jar".toString())
+File runnerFile = new File("${project.basedir}/output/mouser.run".toString())
+runnerFile.append(bashCmdFile.getBytes())
+runnerFile.append(jarFile.getBytes())
+builder = new AntBuilder()
+builder.chmod(file:runnerFile.absolutePath, perm:"+x")
 
-println "creating file createBashWrapper.sh"
-File createBashWrapperFile = new File("${project.basedir}/src/main/scripts/createBashWrapper.sh".toString())
-
-println "writing to file"
-createBashWrapperFile.withWriter('UTF-8') {writer -> writer.write(bashWrapCmdFileText)}
--- Mouser/src/main/scripts/createBashWrapper.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-cat stub.sh ../../../target/Mouser-0.0.1-SNAPSHOT-jar-with-dependencies.jar > ../../../output/mouser.run && chmod +x ../../../output/mouser.run 
